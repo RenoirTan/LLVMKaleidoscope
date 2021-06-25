b9278ef0f1eb5abf18211e4abde938d309ea2444
RenoirTan <renoirtan2005@gmail.com>
2021-06-25 15:50:52 +0800

    main: tokenizer debugger

---
+++ src/bin/tokenizer.rs
@@ -0,0 +1,28 @@
+use std::{
+    convert::TryFrom,
+    env,
+    path::PathBuf
+};
+use kaleidoscope_lexer::{
+    token::TokenKind,
+    tokenizer::{FileStream, Tokenizer}
+};
+
+fn main() {
+    let cmd_args = env::args().collect::<Vec<String>>();
+    let file = match cmd_args.get(1) {
+        Some(path) => {
+            let path = PathBuf::from(path);
+            FileStream::try_from(&*path).unwrap()
+        },
+        None => FileStream::default()
+    };
+    let mut tokenizer = Tokenizer::new(file);
+    loop {
+        let token = tokenizer.next_token().unwrap();
+        println!("{}", token);
+        if let TokenKind::Eof = token.token_kind {
+            break;
+        }
+    }
+}
