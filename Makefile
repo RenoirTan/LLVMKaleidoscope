a83029fcbc00c9f88349c0e61987169683002f54
RenoirTan <renoirtan2005@gmail.com>
2021-06-27 23:12:56 +0800

    main: go to town with main library file and makefile

--- Makefile
+++ Makefile
@@ -1,11 +1,40 @@
 CARGO=cargo
 ARGS=--workspace --color=always
 
-.PHONY: dfull rfull doc dbuild rbuild test
+.PHONY: help all dfull rfull doc dbuild rbuild test
+
+help:
+	@echo " _   __      _      _     _                                "
+	@echo "| | / /     | |    (_)   | |                               "
+	@echo "| |/ /  __ _| | ___ _  __| | ___  ___  ___ ___  _ __   ___ "
+	@echo "|    \\ / _  | |/ _ \\ |/ _\\ |/ _ \\/ __|/ __/ _ \\| '_ \\ / _ \\"
+	@echo "| |\\  \\ (_| | |  __/ | (_| | (_) \\__ \\ (_| (_) | |_) |  __/"
+	@echo "\\_| \\_/\\__,_|_|\\___|_|\\__,_|\\___/|___/\\___\\___/| .__/ \\___|"
+	@echo "                                               | |         "
+	@echo "                                               |_|         "
+	@echo ""
+	@echo "Helper Makefile"
+	@echo ""
+	@echo "USAGE: [TARGET_NAME] [OPTIONS]"
+	@echo ""
+	@echo " - help: Display this help message."
+	@echo " - dbuild: Build library with the debug profile."
+	@echo " - rbuild: Build library with the release profile."
+	@echo " - doc: Generate the documentation for this library."
+	@echo " - test: Run unit tests and doctests."
+	@echo " - dfull: Build library with debug profile, generate docs and test."
+	@echo " - rfull: Build library with release profile, generate docs and test."
+	@echo " - all: Build library in debug and release mode, as well as the documentation and running tests."
+
+
+all: dbuild rbuild test doc
+
 
 dfull: dbuild doc test
 
+
 rfull: rbuild doc test
+
 
 doc:
 	$(CARGO) $@ $(ARGS) --no-deps --document-private-items
--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,49 @@
+//! This is the root page for my implementation of a parser and compiler for
+//! LLVM's Kaleidoscope language
+//! (<https://llvm.org/docs/tutorial/MyFirstLanguageFrontend/index.html>).
+//! I did started following this tutorial out of boredom and am now regretting
+//! this decision for not doing it in C++ like LLVM did in their
+//! documentation.
+//! 
+//! # Purpose of the main library
+//! 
+//! The main library is mainly used to redirect readers of this piece of
+//! documentation to the correct members in the cargo workspace and does not
+//! contain any special functions or types. If I do ever define anything here,
+//! it's usually a re-export or wrapper of one of the functions/types in one
+//! of the other member libraries.
+//! 
+//! # Catalogue
+//! 
+//! In alphabetical order:
+//! 
+//! 1. [`kaleidoscope_ast`] - Defines types that can represent the grammar of
+//! Kaleidoscope as an Abstract Syntax Tree.
+//! 2. [`kaleidoscope_error`] - Defines a custom generic error type which can
+//! be used to fit the purposes of each member library.
+//! 3. [`kaleidoscope_lexer`] - Library which can parses file/stream input
+//! into tokens.
+//! 4. [`kaleidoscope_macro`] - Assortment of macros which I use frequently in
+//! this crate.
 
+pub use kaleidoscope_ast::{
+    nodes,
+    node::Node,
+    NodeId
+};
+pub use kaleidoscope_error::{
+    Error,
+    ErrorKind,
+    Result
+};
+pub use kaleidoscope_lexer::{
+    token::{Token, TokenKind},
+    tokenizer::Tokenizer
+};
+pub use kaleidoscope_macro::{
+    impl_display,
+    hash_map,
+    untrimmed_function_path,
+    function_path,
+    function_name
+};
