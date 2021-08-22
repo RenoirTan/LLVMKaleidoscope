87a45ffd3881fa8deb3318b607e8d5a6677c6d6f
RenoirTan <renoirtan2005@gmail.com>
2021-08-22 21:43:36 +0800

    main: add test for parsing floats

--- src/parser/src/tests/parser_tests.rs
+++ src/parser/src/tests/parser_tests.rs
@@ -1,5 +1,8 @@
 use kaleidoscope_ast::{
-    nodes::IntegerNode,
+    nodes::{
+        FloatNode,
+        IntegerNode
+    },
     node::reify_expr_node
 };
 use kaleidoscope_lexer::{
@@ -11,14 +14,26 @@ use kaleidoscope_lexer::{
 };
 use crate::parser::Parser;
 
+fn get_parser<'a>(input: &'a str) -> (Parser, FileStream<'a>, Tokenizer) {
+    (Parser::new(), FileStream::from(input), Tokenizer::new())
+}
+
 #[test]
 fn test_parse_integer() {
-    let mut parser = Parser::new();
-    let mut stream = FileStream::from("420");
-    let mut tokenizer = Tokenizer::new();
+    let (mut parser, mut stream, mut tokenizer) = get_parser("420");
     let expression = parser.parse_integer_expression(
         ltuplemut!(&mut stream, &mut tokenizer)
     ).unwrap().unwrap();
     let node = reify_expr_node::<IntegerNode>(expression).unwrap();
     assert_eq!(node.get_value(), 420);
+}
+
+#[test]
+fn test_parse_float() {
+    let (mut parser, mut stream, mut tokenizer) = get_parser("3.8");
+    let expression = parser.parse_float_expression(
+        ltuplemut!(&mut stream, &mut tokenizer)
+    ).unwrap().unwrap();
+    let node = reify_expr_node::<FloatNode>(expression).unwrap();
+    assert_eq!(node.get_value(), 3.8);
 }
