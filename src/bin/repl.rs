ad416e35b4552db8ae786c840571c34f37042f05
RenoirTan <renoirtan2005@gmail.com>
2021-09-04 09:53:12 +0800

    main: add press enter to continue
    to repl

--- src/bin/repl.rs
+++ src/bin/repl.rs
@@ -1,3 +1,7 @@
+use std::{
+    convert::AsRef,
+    io::{prelude::*, stdin, stdout}
+};
 use kaleidoscope_parser::driver::Interpreter;
 
 
@@ -7,6 +11,14 @@ Press enter to show the prompt.
 To exit, type in a semicolon (';') without an expression before it."#;
 
 
+fn press_enter_to_continue(prompt: &dyn AsRef<str>) {
+    print!("{}", prompt.as_ref());
+    stdout().flush().unwrap();
+    let mut _dummy = String::new();
+    stdin().read_line(&mut _dummy).unwrap();
+}
+
+
 fn main() {
     kaleidoscope_logging::init(None).unwrap();
 
@@ -14,4 +26,5 @@ fn main() {
     println!("{}", WELCOME_MESSAGE);
     let mut repl = Interpreter::default();
     println!("Statements parsed: {}", repl.main_loop());
+    press_enter_to_continue(&"Press enter to continue::> ");
 }
